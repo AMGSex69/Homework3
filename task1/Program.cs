0a1c89aca6ac70309e05d0d7d2b262e9e0f5197d
Egor <egordolgih@mail.ru>
2022-07-06 21:12:56 +0300

    task1 completed

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+
+void numberCheck(string sign)
+{
+	if (sign[0] == sign[4] || sign[1] == sign[3])
+	{
+		Console.WriteLine("Ваше число - палиндром.");
+	}
+	else Console.WriteLine("Ваше число -  не палиндром.");
+}
+
+if (number.Length == 5)
+{
+	numberCheck(number);
+}
+else Console.WriteLine("Введи пятизначное число");
