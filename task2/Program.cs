e43641c62966e7977268d7b0672b90fe639046d5
Egor <egordolgih@mail.ru>
2022-07-06 21:38:30 +0300

    task2 completed

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿int x1 = Point("x", "A");
+int y1 = Point("y", "A");
+int z1 = Point("z", "A");
+int x2 = Point("x", "B");
+int y2 = Point("y", "B");
+int z2 = Point("z", "B");
+
+int Point(string cordinateName, string pointName)
+{
+	Console.Write($"Введите координату {cordinateName} точки {pointName}: ");
+	return Convert.ToInt32(Console.ReadLine());
+}
+
+double Question(double x1, double x2,
+					 double y1, double y2,
+					 double z1, double z2)
+{
+	return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+}
+
+double segmentLength = Math.Round(Question(x1, x2, y1, y2, z1, z2), 3);
+
+Console.WriteLine("Длина отрезка  {segmentLength}");
